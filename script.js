39bbb9ea286b82f752bd7aea0c9f262f33bbb3bf
Bhushan pandagre <dengercoder790@gmail.com>
2024-04-22 12:46:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+var btn = document.querySelector("button");
+
+btn.addEventListener("click",function(){
+  // document.querySelector("#fs").style.display = "initial";
+  document.querySelector("#fs").style.opacity = 1;
+  document.querySelector("#modal").style.top = "15%";
+
+})
